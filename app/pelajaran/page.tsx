2f09b2e560aa9cc584a00906637a9990dc34d413
12rekayasa2-rpl <12rekayasa2@gmail.com>
2026-06-14 11:41:06 +0800

    tambah konten

--- app/pelajaran/page.tsx
+++ app/pelajaran/page.tsx
@@ -81,7 +81,7 @@ const pelajaranPage = () => {
         </div>
         
         <div className="max-w-7xl mx-auto px-4 py-12 sm:px-6 lg:px-8">
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg-grid-cols-3 gap-8">
             {daftarPelajaran.map((pelajaran) => (
               <div 
                 key={pelajaran.id} 
